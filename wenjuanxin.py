eaef544bfc248f263e8787536a8499cb97b327df
misschildhood <923219711@qq.com>
2020-04-04 01:00:59 +0800

    [功能]cookie自搜寻

--- wenjuanxin.py
+++ wenjuanxin.py
@@ -21,13 +21,14 @@ headers = {
     'Content-Type': 'text/plain'
 }
 
+
 def current_time(a):
     '''
     a:秒数
     返回当前时间
     :return:
     '''
-    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()-a))
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time() - a))
 
 
 def random_num(min, max):
@@ -41,7 +42,7 @@ def random_num(min, max):
 
 
 def random_parameter():
-    str='curid=69156112'
+    str = 'curid=69156112'
     '''
     随机请求参数
     :return:
@@ -57,25 +58,42 @@ def random_parameter():
         jqnonce: 61a28b7e-460b-4762-8b49-783d09ec1e09
         jqsign: 70`39c6d,571c,5673,9c58,692e18db0d18
     '''
-    starttime=current_time(random_num(10,50))
-    now_time=time.time()
-    t=int(now_time*1000)
-    source='directphone'
-    submittype=1
-    ktimes=random_num(200,575)
-    hlv=1
-    rn='2027509235.{}'.format(int(math.modf(now_time)[0]*10000000))
-    jpm=17
-
+    starttime = current_time(random_num(10, 50))
+    now_time = time.time()
+    t = int(now_time * 1000)
+    source = 'directphone'
+    submittype = 1
+    ktimes = random_num(200, 575)
+    hlv = 1
+    rn = '2027509235.{}'.format(int(math.modf(now_time)[0] * 10000000))
+    jpm = 17
 
     return
+
+
+def jqsignAndjqnonce():
+    url = "https://www.wjx.cn/m/69156112.aspx"
+    head = {
+        # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Connection': 'keep-alive',
+        'Cookie': 'acw_tc=2f624a4c15859126853877402e62d4a0ffe71fe04433f4f22a1f6fc25631a9; .ASPXANONYMOUS=ZNpKYjtA1gEkAAAAMjIyMzdhMWEtOWI4ZS00ZjZhLTllM2YtNDk2NTQxM2RiNGY2RrlH4oMvK38t4FPe1gv1zc2TWy81; SERVERID=0f3eb8fcde19feef85b46d49c555413b|1585933053|1585933053; Hm_lvt_21be24c80829bd7a683b2c536fcf520b=1585912686; Hm_lpvt_21be24c80829bd7a683b2c536fcf520b=1585933055; UM_distinctid=1713fc327b1155-075cd0241399148-4c302f7e-144000-1713fc327b2328; CNZZDATA4478442=cnzz_eid%3D732218403-1585910396-%26ntime%3D1585931996; jac69156112=45696809',
+        'Upgrade-Insecure-Requests': '1',
+        'Cache-Control': 'max-age=0',
+        'Cookie': 'SERVERID=6142ed0ee68ecc71fb491c53c82ec4a0|1585921170|1585921170'
+    }
+    res = requests.get(url)
+    print(res.cookies)
+
+    soup = BeautifulSoup(res.text, 'html.parser')
+    # print(res.text)
+    rndnum = re.search("rndnum=\".+\";", res.text).group(0)[8:-2]
+    jqnonce = re.search("jqnonce=\".+\";", res.text).group(0)[9:-2]
 
 
 def dataenc(ktimes):
-
-
     return
-
 
 
 def random_url():
@@ -89,22 +107,24 @@ def random_url():
 ua = UserAgent()
 
 if __name__ == '__main__':
-    res=requests.get("https://www.wjx.cn/m/69156112.aspx",headers=headers)
+    res = requests.get("https://www.wjx.cn/m/69156112.aspx")
+    print(res.cookies)
 
-    soup=BeautifulSoup(res.text, 'html.parser')
+    soup = BeautifulSoup(res.text, 'html.parser')
     # print(res.text)
     print(re.search("rndnum=\".+\";", res.text).group(0)[8:-2])
+    print(re.search("jqnonce=\".+\";", res.text).group(0)[9:-2])
     # print(re.search("rndnum=.*'", res))
     url = "https://www.wjx.cn/joinnew/processjq.ashx?"
     # print(current_time(0))
     # print(current_time(3000))
-    a=123.456
+    a = 123.456
 
-    time_str=time.time()
+    time_str = time.time()
     b = math.modf(time_str)[0]
-    print(int(b*10000000))
+    print(int(b * 10000000))
     # print(time.time())
-    print(int(time.time()*1000))
+    print(int(time.time() * 1000))
     # print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())))
     # print(random_num(150, 350))
     print()
