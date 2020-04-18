56565161356fc7be5472396ae1febba67d01fb2a
misschildhood <923219711@qq.com>
2020-04-18 23:17:55 +0800

    [增加]qq批量投票

--- AuthorQq.py
+++ AuthorQq.py
@@ -3,3 +3,22 @@
 # @Author  : lyh-god
 # @FileName: AuthorQq.py
 # @Software: PyCharm
+class AuthorQq:
+    '''
+        public String authorize() {
+            return "https://graph.qq.com/oauth2.0/authorize";
+        }
+        public String accessToken() {
+            return "https://graph.qq.com/oauth2.0/token";
+        }
+        public String userInfo() {
+            return "https://graph.qq.com/user/get_user_info";
+        }
+        public String refresh() {
+            return "https://graph.qq.com/oauth2.0/token";}
+    '''
+
+
+
+if __name__ == '__main__':
+    print()
